ac8eb083877513c0f3fd969218636a51f5fb8baa
FateZeros <490293266@qq.com>
2018-05-29 11:32:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## 参考文档
+[官方文档](https://golang.org/doc/) </br>
+[GitHub上优秀的Go开源项目](http://www.flysnow.org/2016/12/27/golang-hot-project-in-github.html)
---
+++ src/github.com/fatezeros/hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatezeros/string"
+)
+
+func main() {
+	fmt.Println(string.Reverse("Hello, gopher \n"))
+}
